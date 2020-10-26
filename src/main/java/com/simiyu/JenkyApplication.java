9e04f9fc166c3f425789bb82b90d4ef71cc967ef
enock <simiyuenock1990@gmail.com>
2020-10-26 20:50:31 +0300

    first commit

---
+++ src/main/java/com/simiyu/JenkyApplication.java
@@ -0,0 +1,13 @@
+package com.simiyu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JenkyApplication.class, args);
+	}
+
+}
